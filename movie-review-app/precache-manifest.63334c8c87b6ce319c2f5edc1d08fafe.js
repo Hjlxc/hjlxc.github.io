8d9bd6391a1d56ea6f318bf36e2d45cc1e721eea
Hjlxc <xcliu0324@gmail.com>
2020-08-19 12:04:20 -0400

    movie-review-app v1

---
+++ movie-review-app/precache-manifest.63334c8c87b6ce319c2f5edc1d08fafe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb13b1f392c0226463905c7ae1bbdf1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f46480f37fbad8f2dcb",
+    "url": "./static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "696b1f5dc199741e8a1d",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6f46480f37fbad8f2dcb",
+    "url": "./static/js/2.f20e3a58.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f20e3a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696b1f5dc199741e8a1d",
+    "url": "./static/js/main.e68abbb0.chunk.js"
+  },
+  {
+    "revision": "a43cef3040433095abf0",
+    "url": "./static/js/runtime-main.528e7ba5.js"
+  }
+]);
